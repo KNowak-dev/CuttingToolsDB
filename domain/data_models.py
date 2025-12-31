2ddcf70c031d1bcea2a1fe27d7cee6f4dd3eced2
KNowak-dev <kn92@op.pl>
2025-12-31 13:50:57 +0100

    Dodany plik data_models.py

---
+++ domain/data_models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional
+from edv_number import EDVNumber
+from semi_edv_number import EDVNumberSemiProduct
+
+@dataclass
+class CuttingTool:
+        nr_edv: Optional[EDVNumber] = None
+        type_tool: str =""
+        origin: str = ""
+        nr_edv_semi_finished_product: Optional[EDVNumberSemiProduct] = None
+        kind_tool: str = ""
+        production_detail: str = ""
+        machine: str = ""
+        production_machine: str = ""
+        semi_finished_product_price: float = 0.0
+        total_price: float = 0.0
+
+@dataclass    
+class data_to_AddCuttingTool:
+    type_tool: str
+    origin: str
+    kind_tool: str
+    production_detail: str
+    machine: str
+    production_machine: str
+    semi_finished_product_price: float
+    total_price: float
+    nr_edv: Optional[EDVNumber] = None
+    nr_edv_semi_finished_product: Optional[EDVNumberSemiProduct] = None
