580ed1351a3735a77ecc0d16f30d908c4bacaa1d
KNowak-dev <kn92@op.pl>
2025-12-31 13:54:22 +0100

    Zmiana struktury magazynu narzędzi

--- infrastructure/tools_storage.py
+++ infrastructure/tools_storage.py
@@ -1,15 +1,29 @@
-from dataclasses import dataclass
+from data_models import CuttingTool
+from edv_number import EDVNumber
 
-# Magazyn/Baza wszystkich narzędzi, które zostały dodane - magazynem będzie lista, natomiast "obiekt" narzędzie jest dataclassą.
+class EDVNumberAlreadyInUseError(Exception):
+    pass
 
-@dataclass
 class ToolStorage:
-    storage = []
+    def __init__(self):
+        self._tools_by_edv: dict[str, CuttingTool] = {}
 
-    @classmethod
-    def add_tool(cls, tool):
-        cls.storage.append(tool)
+    def add(self, tool: CuttingTool):
+        if tool.nr_edv in self._tools_by_edv:
+            raise EDVNumberAlreadyInUseError(f"EDV {tool.nr_edv} is already used")
+        self._tools_by_edv[tool.nr_edv] = tool
 
+    def exists_by_edv(self, edv: EDVNumber) -> bool:
+            return edv in self._tools_by_edv
+    
+    def get(self, edv: EDVNumber) -> CuttingTool | None:
+        return self._tools_by_edv.get(edv)
+
+    def all(self):
+        return list(self._tools_by_edv.values())
+        
 if __name__ == "__main__":
     storage = ToolStorage()
-    
+
+    lista_narzedzi = storage.all()
+    print(lista_narzedzi)
