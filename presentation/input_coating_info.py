78f5357e55184bd7c9df5e20d6979f27ded0e820
KNowak-dev <kn92@op.pl>
2026-01-02 17:53:47 +0100

    Dodałem moduł wprowadzania danych przez użytkownika do modułu powłoki

---
+++ presentation/input_coating_info.py
@@ -0,0 +1,14 @@
+def Input_Coating_name():
+    coating_name = str(input("Wprowadź nazwę powłoki:"))
+
+    return coating_name
+
+def Input_Coating_Width():
+    coating_width = str(input("Wprowadź grubość powłoki:"))
+
+    return coating_width
+
+def Input_Coating_Factory():
+    coating_factory = str(input("Wprwoadź nazwę fimry powlekającej:"))
+
+    return coating_factory
