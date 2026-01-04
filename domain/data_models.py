73147fa3f977e52f7f4f0db064a06bacd05af463
KNowak-dev <kn92@op.pl>
2026-01-04 19:24:57 +0100

    Zmiany w data_models przez dodanie pola ilość narzędzi oraz zmiana typu pola tool_type, ktory od tej pory jest wybierany przy pomocy klasy i enum a nie zwykłego inputu

--- domain/data_models.py
+++ domain/data_models.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Optional
 from edv_number import EDVNumber
 from semi_edv_number import EDVNumberSemiProduct
+from choose_type import InputTypeTool
 
 @dataclass
 class CoatingTool:
@@ -12,7 +13,7 @@ class CoatingTool:
 @dataclass
 class CuttingTool:
         nr_edv: Optional[EDVNumber] = None
-        type_tool: str =""
+        type_tool: Optional[str] = None
         origin: str = ""
         nr_edv_semi_finished_product: Optional[EDVNumberSemiProduct] = None
         kind_tool: str = ""
@@ -21,11 +22,12 @@ class CuttingTool:
         production_machine: str = ""
         semi_finished_product_price: float = 0.0
         total_price: float = 0.0
+        quantity: int = 0
         coating: Optional[CoatingTool] = None
         
 @dataclass
 class data_to_AddCuttingTool:
-    type_tool: str
+    type_tool: Optional[InputTypeTool] = None
     origin: str
     kind_tool: str
     production_detail: str
@@ -33,5 +35,6 @@ class data_to_AddCuttingTool:
     production_machine: str
     semi_finished_product_price: float
     total_price: float
+    quantity: int
     nr_edv: Optional[EDVNumber] = None
     nr_edv_semi_finished_product: Optional[EDVNumberSemiProduct] = None
