47aa9a030a34eff3975b7f1a0ce003345a0eb551
KNowak-dev <kn92@op.pl>
2026-01-06 17:43:10 +0100

    edycja pliku edit_mappings.py

--- application/edit_mappings.py
+++ application/edit_mappings.py
@@ -1,8 +1,6 @@
 from edit_tool import EditOption
 
-class EditOptionToField: 
-    def __init__(self):
-        self.field_map: dict[EditOption, str] = {EditOption.TYPE_TOOL: "type_tool",
+EDIT_OPTION_TO_FIELD_MAP:dict[EditOption, str] = {EditOption.TYPE_TOOL: "type_tool",
                                                 EditOption.ORIGIN: "origin",
                                                 EditOption.NR_EDV_SEMI_FINISHED_PRODUCT: "nr_edv_semi_finished_product",
                                                 EditOption.KIND_TOOL: "kind_tool",
