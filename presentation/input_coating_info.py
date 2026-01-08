41f834b0494c408600e033259d6bc92c19c090c5
KNowak-dev <kn92@op.pl>
2026-01-08 13:17:49 +0100

    Zmiany w pliku podyktowane dodaniem atrybutu ceny powłoki

--- presentation/input_coating_info.py
+++ presentation/input_coating_info.py
@@ -4,16 +4,39 @@ def Input_Coating_name():
     return coating_name
 
 def Input_Coating_Width():
-    coating_width = str(input("Wprowadź grubość powłoki:"))
-
-    return coating_width
-
+    while True:
+        try:
+            coating_width = int(input("Wprowadź grubość powłoki: "))
+            if coating_width < 0:
+                print("Ilość nie może być ujemna.")
+                continue
+            return coating_width
+        except ValueError:
+            print("Wprowadź poprawną liczbę całkowitą.")
+    
 def Input_Coating_Factory():
     coating_factory = str(input("Wprwoadź nazwę fimry powlekającej:"))
 
     return coating_factory
 
 def Input_coating_in_progress():
-    coating_in_progress = int(input("Wprowadź iloś narzędzi wysłanej do powlekania:"))
+    while True:
+        try:
+            coating_in_progress = int(input("Wprowadź ilość narzędzi wysłanych do powlekania: "))
+            if coating_in_progress < 0:
+                print("Ilość nie może być ujemna.")
+                continue
+            return coating_in_progress
+        except ValueError:
+            print("Wprowadź poprawną liczbę całkowitą.")
 
-    return coating_in_progress
+def Input_coating_price():
+    while True:
+        try:
+            coating_price = float(input("Wprowadź cenę powlekania:"))
+            if coating_price < 0:
+                print("Ilość nie może być ujemna.")
+                continue
+            return coating_price
+        except ValueError:
+            print("Wprowadź poprawną liczbę całkowitą.")
